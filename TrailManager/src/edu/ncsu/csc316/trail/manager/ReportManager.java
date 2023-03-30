ae374d43fd11d3616e87cafac3596f675c076e25
Jimin Yu <jyu34@ncsu.edu>
2023-03-30 14:39:29 -0400

    [Implementation] Implemented UI, seems to work as intended. Everything still needs unit testing

--- TrailManager/src/edu/ncsu/csc316/trail/manager/ReportManager.java
+++ TrailManager/src/edu/ncsu/csc316/trail/manager/ReportManager.java
@@ -14,132 +14,152 @@ import edu.ncsu.csc316.trail.dsa.DSAFactory;
 import edu.ncsu.csc316.trail.dsa.DataStructure;
 
 public class ReportManager {
-	
-	public static final int MILE = 5280;
-	
+
+	public static final double MILE = 5280.0;
+	public static final double ROUND = 100.0;
+
 	private Sorter<Entry<Landmark, Integer>> distanceSorter;
 	private Sorter<Entry<Landmark, List<Trail>>> intersectSorter;
-	
+
 	private TrailManager t;
-	
 
-    public ReportManager(String pathToLandmarkFile, String pathToTrailFile) throws FileNotFoundException {
-        // TODO: FIRST set the data structures and algorithms you will need to use for your solution.
-        // For example, for Maps: DSAFactory.setMapType(DataStructure.<MAPTYPE>)
-        // You'll need to set any Maps, Lists, and/or sorting algorithms that are used.
-        // See the project writeup for more information about using DSAFactory.
-        // This is the ONLY place you will need to call these DSAFactory setter methods!
-        
-        // TODO: Complete this constructor
-    	
-    	DSAFactory.setMapType(DataStructure.UNORDEREDLINKEDMAP);
-    	DSAFactory.setListType(DataStructure.SINGLYLINKEDLIST);
-    	DSAFactory.setComparisonSorterType(Algorithm.MERGESORT);
-    	
-    	t = new TrailManager(pathToLandmarkFile, pathToTrailFile);
-        
-    }
+	public ReportManager(String pathToLandmarkFile, String pathToTrailFile) throws FileNotFoundException {
 
-    public String getDistancesReport(String originLandmark) {
-    	
-    	Map<Landmark, Integer> distanceMap = t.getDistancesToDestinations(originLandmark);
-    	Entry<Landmark, Integer>[] sortedList = distanceSort(distanceMap);
-    	
-    	String output = "";
-    	output += "Landmarks Reachable from " + sortedList[0].getKey().getDescription() + " (" + originLandmark + ") {\n";
-    	for (int i = 1; i < sortedList.length; i++) {
-    		if (sortedList[i].getValue() > MILE) {
-    			double miles = sortedList[i].getValue() / MILE;
-    			output += "\t" + sortedList[i].getValue() + " feet (" + miles + " miles) to " + sortedList[i].getKey().getDescription() + " (" + sortedList[i].getKey().getId() + ")\n";
-    		}
-    		else {
-    			output += "\t" + sortedList[i].getValue() + " feet to " + sortedList[i].getKey().getDescription() + " (" + sortedList[i].getKey().getId() + ")\n";
-    		}
-    	}
-    	output += "}";
-    	return output;
-    }
+		DSAFactory.setMapType(DataStructure.UNORDEREDLINKEDMAP);
+		DSAFactory.setListType(DataStructure.SINGLYLINKEDLIST);
+		DSAFactory.setComparisonSorterType(Algorithm.MERGESORT);
 
-    public String getProposedFirstAidLocations(int numberOfIntersectingTrails) {
-    	Map<Landmark, List<Trail>> intersectMap = t.getProposedFirstAidLocations(numberOfIntersectingTrails);
-    	Entry<Landmark, List<Trail>>[] sortedList = intersectSort(intersectMap);
-    	
-    	String output = "";
-    	output += "Proposed Locations for First Aid Stations {\n";
-    	for (int i = 0; i < sortedList.length; i++) {
-    		if (sortedList[i].getValue().size() >= numberOfIntersectingTrails) {
-    			output += "\t" + sortedList[i].getKey().getDescription() + " (" + sortedList[i].getKey().getId() + ") - " + sortedList[i].getValue().size() + " intersecting trails\n";
-    		}
-    	}
-    	output += "\n";
-    	return output;
-    }
-    
-    @SuppressWarnings("unchecked")
+		t = new TrailManager(pathToLandmarkFile, pathToTrailFile);
+
+	}
+
+	public String getDistancesReport(String originLandmark) {
+		if (t.getLandmarkByID(originLandmark) == null) {
+			return "The provided landmark ID (" + originLandmark + ") is invalid for the park.";
+		}
+
+		Map<Landmark, Integer> distanceMap = t.getDistancesToDestinations(originLandmark);
+		Entry<Landmark, Integer>[] sortedList = distanceSort(distanceMap);
+
+		if (sortedList.length == 1 && sortedList[0].getKey().getId().equals(originLandmark)) {
+			return "No landmarks are reachable from " + originLandmark + ".";
+		}
+
+		String output = "";
+		output += "Landmarks Reachable from " + sortedList[0].getKey().getDescription() + " (" + originLandmark
+				+ ") {\n";
+		for (int i = 1; i < sortedList.length; i++) {
+			if (sortedList[i].getValue() > MILE) {
+				double miles = sortedList[i].getValue() / MILE;
+				miles = Math.round(miles * ROUND) / ROUND;
+				output += "   " + sortedList[i].getValue() + " feet (" + miles + " miles) to "
+						+ sortedList[i].getKey().getDescription() + " (" + sortedList[i].getKey().getId() + ")\n";
+			} else {
+				output += "   " + sortedList[i].getValue() + " feet to " + sortedList[i].getKey().getDescription() + " ("
+						+ sortedList[i].getKey().getId() + ")\n";
+			}
+		}
+		output += "}";
+		return output;
+	}
+
+	public String getProposedFirstAidLocations(int numberOfIntersectingTrails) {
+		if (numberOfIntersectingTrails <= 0) {
+			return "Number of intersecting trails must be greater than 0.";
+		}
+
+		Map<Landmark, List<Trail>> intersectMap = t.getProposedFirstAidLocations(numberOfIntersectingTrails);
+		Entry<Landmark, List<Trail>>[] sortedList = intersectSort(intersectMap);
+
+		if (sortedList.length == 0) {
+			return "No landmarks have at least " + numberOfIntersectingTrails + " intersecting trails";
+		}
+
+		String output = "";
+		output += "Proposed Locations for First Aid Stations {\n";
+		for (int i = 0; i < sortedList.length; i++) {
+			if (sortedList[i].getValue().size() >= numberOfIntersectingTrails) {
+				output += "   " + sortedList[i].getKey().getDescription() + " (" + sortedList[i].getKey().getId()
+						+ ") - " + sortedList[i].getValue().size() + " intersecting trails\n";
+			}
+		}
+		output += "}";
+		return output;
+	}
+
+	@SuppressWarnings("unchecked")
 	private Entry<Landmark, Integer>[] distanceSort(Map<Landmark, Integer> map) {
 		DistanceComparator c = new DistanceComparator();
 		distanceSorter = DSAFactory.getComparisonSorter(c);
 
 		List<Entry<Landmark, Integer>> list = DSAFactory.getIndexedList();
-		for (Entry<Landmark, Integer> entry: map.entrySet()) {
+		for (Entry<Landmark, Integer> entry : map.entrySet()) {
 			list.addLast(entry);
 		}
+
+		Entry<Landmark, Integer>[] e = new Entry[list.size()];
 		
-		Entry<Landmark, Integer>[] e = (Entry<Landmark, Integer>[]) (new Object[list.size()]);
-		for (int i = 0; i < list.size(); i++) {
-			e[i] = list.get(i);
+		int i = 0;
+		for (Entry<Landmark, Integer> entry: list) {
+			e[i] = entry;
+			i++;
 		}
+
 		distanceSorter.sort(e);
 		return e;
 	}
-    
-    @SuppressWarnings("unchecked")
+
+	@SuppressWarnings("unchecked")
 	private Entry<Landmark, List<Trail>>[] intersectSort(Map<Landmark, List<Trail>> map) {
-    	IntersectComparator c = new IntersectComparator();
-    	 intersectSorter = DSAFactory.getComparisonSorter(c);
-    	 
-    	 List<Entry<Landmark, List<Trail>>> list = DSAFactory.getIndexedList();
-    	 for (Entry<Landmark, List<Trail>> entry: map.entrySet()) {
-    		 list.addLast(entry);
-    	 }
-    	 
-    	 Entry<Landmark, List<Trail>>[] e = (Entry<Landmark, List<Trail>>[]) (new Object[list.size()]);
-    	 for (int i = 0; i < list.size(); i++) {
-    		 e[i] = list.get(i);
-    	 }
-    	 intersectSorter.sort(e);
-    	 return e;
-    }
-    
-	private class DistanceComparator implements Comparator<Entry<Landmark, Integer>> {
+		IntersectComparator c = new IntersectComparator();
+		intersectSorter = DSAFactory.getComparisonSorter(c);
+
+		List<Entry<Landmark, List<Trail>>> list = DSAFactory.getIndexedList();
+		for (Entry<Landmark, List<Trail>> entry : map.entrySet()) {
+			list.addLast(entry);
+		}
+
+//		Entry<Landmark, List<Trail>>[] e = (Entry<Landmark, List<Trail>>[]) (new Object[list.size()]);
+//		for (int i = 0; i < list.size(); i++) {
+//			e[i] = list.get(i);
+//		}
+		Entry<Landmark, List<Trail>>[] e = new Entry[list.size()];
+		int i = 0;
+		for (Entry<Landmark, List<Trail>> entry: list) {
+			e[i] = entry;
+			i++;
+		}
 		
+		intersectSorter.sort(e);
+		return e;
+	}
+
+	private class DistanceComparator implements Comparator<Entry<Landmark, Integer>> {
+
 		@Override
 		public int compare(Entry<Landmark, Integer> one, Entry<Landmark, Integer> two) {
 			if (one.getValue() < two.getValue()) {
 				return -1;
-			}
-			else if (one.getValue() > two.getValue()) {
+			} else if (one.getValue() > two.getValue()) {
 				return 1;
-			}
-			else {
+			} else {
 				return one.getKey().getDescription().compareTo(two.getKey().getDescription());
 			}
 		}
 	}
-	
+
 	private class IntersectComparator implements Comparator<Entry<Landmark, List<Trail>>> {
 
 		@Override
 		public int compare(Entry<Landmark, List<Trail>> one, Entry<Landmark, List<Trail>> two) {
 			if (one.getValue().size() > two.getValue().size()) {
 				return -1;
-			}
-			else if (one.getValue().size() < two.getValue().size()) {
+			} else if (one.getValue().size() < two.getValue().size()) {
 				return 1;
 			}
 			return one.getKey().getDescription().compareTo(two.getKey().getDescription());
 		}
-		
+
 	}
-    
+
 }
--- TrailManager/src/edu/ncsu/csc316/trail/ui/TrailReportUI.java
+++ TrailManager/src/edu/ncsu/csc316/trail/ui/TrailReportUI.java
@@ -5,17 +5,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import edu.ncsu.csc316.trail.manager.ReportManager;
+
 public class TrailReportUI {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Scanner in = new Scanner(System.in);
 		String landmarkFile;
+		String trailFile;
 		File file;
+		System.out.println("Enter the path to the file containing landmark information: ");
 		do {
-			System.out.println("Enter the path to the file containing landmark information: ");
 			landmarkFile = in.nextLine();
+			if (landmarkFile.equals("quit")) {
+				in.close();
+				System.exit(0);
+			}
 			file = new File(landmarkFile);
 			try (Scanner scan = new Scanner(new FileInputStream(landmarkFile), "UTF8")) {
-				
+				break;
 			} catch (FileNotFoundException e) {
 				System.out.println("Invalid file path, please try again: ");
 				continue;
@@ -23,8 +30,51 @@ public class TrailReportUI {
 			
 		} while (!file.exists());
 		
+		System.out.println("Enter the path to the file containing trail information: ");
+		do {
+			trailFile = in.nextLine();
+			if (trailFile.equals("quit")) {
+				in.close();
+				System.exit(0);
+			}
+			file = new File(trailFile);
+			try (Scanner scan = new Scanner(new FileInputStream(trailFile), "UTF8")) {
+				break;
+			} catch (FileNotFoundException e) {
+				System.out.println("Invalid file path, please try again: ");
+				continue;
+			}
+		} while (!file.exists());
 		
+		
+		System.out.println("\n");
+		
+		ReportManager r = new ReportManager(landmarkFile, trailFile);
+		
+		System.out.println("Would you like a report of proposed first aid locations (enter \"first aid\") or a distance report (enter \"distance\")? ");
+		String reportType = in.nextLine();
 
+		if (reportType.equals("first aid")) {
+			System.out.println("Please input the desired minimum number of intersecting trails: ");
+			int numIntersect = Integer.parseInt(in.nextLine());
+			System.out.println(r.getProposedFirstAidLocations(numIntersect));
+		}
+		else if (reportType.equals("distance")) {
+			System.out.println("Please input the landmark id of the starting point: ");
+			String id = in.nextLine();
+			System.out.println(r.getDistancesReport(id));
+		}
+		else if (reportType.equals("quit")) {
+			in.close();
+			System.exit(0);
+		}
+		else {
+			System.out.println("Invalid input.");
+			in.close();
+			System.exit(1);
+		}
 		
+		in.close();
+		System.exit(0);	
 	}
 }
